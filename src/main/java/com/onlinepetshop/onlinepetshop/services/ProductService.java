ef3f15c3ef8498a996a93c026615cdbfd2cc9bd2
Maksym Mahera <maksmagera2@gmail.com>
2023-11-19 21:43:39 +0200

    upd

--- src/main/java/com/onlinepetshop/onlinepetshop/services/ProductService.java
+++ src/main/java/com/onlinepetshop/onlinepetshop/services/ProductService.java
@@ -1,13 +1,19 @@
 package com.onlinepetshop.onlinepetshop.services;
 
 import com.onlinepetshop.onlinepetshop.models.Product;
+import com.onlinepetshop.onlinepetshop.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
     private List<Product> products = new ArrayList<>();
     private long ID = 0;
 
@@ -19,24 +25,21 @@ public class ProductService {
                 70000,"Grothers","Dunaivtsi"));
     }
 
-    public List<Product> listProducts(){
-        return products;
+    public List<Product> listProducts(String title){
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
     public void saveProducts(Product product){
-        product.setId(++ID);
-        products.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
     public void deleteProduct(Long id){
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id))
-                return product;
-        }
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 
 }
